efd20dbd47064ac8b36a62c82d98f672ff41ba02
Geethik Narayana K <gn1581@tamu.edu>
2017-04-28 05:14:11 +0000

    Bug fix for Relevance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,11 +95,11 @@ class UsersController < ApplicationController
             if (!params[:selected_keys_save].nil? and params[:selected_keys_save].include?(k))
               @storage[k] = [v[0],v[1],v[2],v[3],v[4],"checked",params[:reasons][i]]
             else
-              if v[5]!="checked"
+              #if v[5]!="checked"
                 @storage[k] = [v[0],v[1],v[2],v[3],v[4],"unchecked",params[:reasons][i]]
-              else
-                @storage[k] = [v[0],v[1],v[2],v[3],v[4],"checked",params[:reasons][i]]
-              end
+             # else
+             #   @storage[k] = [v[0],v[1],v[2],v[3],v[4],"checked",params[:reasons][i]]
+             # end
             end
             i=i+1
           end
